6028880c8b6125ccc592c9b1be6fc153d2362cdb
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-05-13 09:41:53 -0300

    segundaparte

--- tarefa02/exemplo/urls.py
+++ tarefa02/exemplo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('another/', views.index, name="outra"),
+    path('terceira/', views.index, name="terceira"),
 ]
--- tarefa02/exemplo/views.py
+++ tarefa02/exemplo/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
+    print (request.method)
+    print (request.META["HTTP_USER_AGENT"])
+    print (f"Bem Vindo{ request.GET.get('nome')}")
+    print (request.GET)
     return render(request, "index.html")
+    
+def outra(request):
+    return render(request,"outra.html")
+
+def terceira(request):
+    return redirect('outra')
