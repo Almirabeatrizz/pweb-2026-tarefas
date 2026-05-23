43e57b2a8d7a4d9c47467120167a6640ce97df5f
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-05-23 13:59:47 -0300

    tarefa

--- tarefa02/exemplo/views.py
+++ tarefa02/exemplo/views.py
@@ -3,37 +3,3 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
     
-def usuarios(request):
-    lista_usuarios = [
-        {
-            'nome' : 'Gabriely Marcelino',
-            'matricula' : '2024114',
-            'idade' : '18', 
-            'cidade' : 'Bom Jesus',
-        },
-        {
-            'nome' : 'Emanuelly Maria',
-            'matricula' : '2023118',
-            'idade' : '18', 
-            'cidade' : 'Bom Jesus',
-        }, 
-        {
-            'nome' : 'Joao victor',
-            'matricula' : '2023113',
-            'idade' : '19', 
-            'cidade' : 'Natal',
-        },
-        {
-            'nome' : 'Sandrielly Oliveira ',
-            'matricula' : '2023117',
-            'idade' : '19', 
-            'cidade' : 'Natal',
-        },
-            {
-            'nome' : 'Joyce Kelly ',
-            'matricula' : '2025114',
-            'idade' : '16', 
-            'cidade' : 'Parnamirim',
-        }
-    ]
-    return render(request,'usuarios.html', {'usuarios': lista_usuarios})
